6034dae653a72e7c1758df61e388bbb16b88fe17
Cemre <44120900+cemreefe@users.noreply.github.com>
2023-04-26 22:56:07 +0000

    Add code

---
+++ generate.py
@@ -0,0 +1,36 @@
+import os
+import emoji
+from PIL import Image, ImageFont
+from pilmoji import Pilmoji
+
+# Get a list containing all the available emojis
+emojis = list(emoji.EMOJI_DATA.keys())
+
+print(len(emojis))
+
+for size in [16]:
+
+    directory = f"./png/{size}x{size}"
+    os.makedirs(directory, exist_ok=True)
+
+    for e in emojis[:2000]:
+
+        try:
+
+            filename = f"{directory}/{e}.png"
+
+            if os.path.exists(filename):
+                continue
+
+            with Image.new('RGBA', (size, size), (0, 0, 0, 0)) as image:
+                font = ImageFont.truetype('Arial.ttf', size)
+
+                with Pilmoji(image) as pilmoji:
+                    pilmoji.text((0, 0), e, (0, 0, 0), font)
+
+                # Save the image as a PNG file
+                image.save(filename)
+        
+        except Exception:
+
+            print("Couldn't process emoji: ", e)
